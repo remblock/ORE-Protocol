4d37705fc6bb777e01a9ac45351143b6b477ce11
SSDx <60996729+ssdx-remblock@users.noreply.github.com>
2020-08-27 14:05:55 +0930

    Update ore-take-snapshot.sh

--- ore-take-snapshot.sh
+++ ore-take-snapshot.sh
@@ -4,19 +4,19 @@
 #                                     ORE-PROTOCOL-TAKE-SNAPSHOT                                     #
 #****************************************************************************************************#
 
-datafolder=/root/data
-statehistoryfolder=$datafolder/state-history
-blocksfolder=$datafolder/blocks
-snapshotsfolder=$datafolder/snapshots
-compressedfolder=$snapshotsfolder/compressed/
+data_folder=/root/data
+blocks_folder=$datafolder/blocks
+snapshots_folder=$datafolder/snapshots
+state_history_folder=$datafolder/state-history
+compressed_folder=$snapshotsfolder/compressed/
 
 #****************************************************************************************************#
 #                                    SCRIPT CONFIGURATION VARIABLES                                  #
 #****************************************************************************************************#
 
-shcreate=$snapshotsfolder/compressandsendlastsnapshot.sh
-shcreatefull=$snapshotsfolder/compressandsendlastsnapshot_full.sh
-shcreatefullstate=$snapshotsfolder/compressandsendlastsnapshot_fullstate.sh
+sh_create=$snapshots_folder/compress_and_send_lastsnapshot.sh
+sh_create_full=$snapshots_folder/compress_and_send_lastsnapshot_full.sh
+sh_create_fullstate=$snapshots_folder/compress_and_send_lastsnapshot_fullstate.sh
 
 #****************************************************************************************************#
 #                                     REMOTE SERVER PARAMETERS                                       #
@@ -29,21 +29,21 @@ remote_user=root@ore.remblock.io
 #                                          MISC PARAMETERS                                           #
 #****************************************************************************************************#
 
-sshport=22
-testblocks=0
-chainstopped=0
-testsnapshot=0
-teststatehistory=0
-thehour=$(date +"%-H")
-datename=$(date +%Y-%m-%d_%H-%M)
-filename="$compressedfolder$datename"
+ssh_port=22
+test_blocks=0
+chain_stopped=0
+test_snapshot=0
+test_state_history=0
+the_hour=$(date +"%-H")
+date_name=$(date +%Y-%m-%d_%H-%M)
+file_name="$compressedfolder$datename"
 
 #----------------------------------------------------------------------------------------------------#
 # CREATE THE DIRECTORY IF IT DOES NOT EXIST                                                          #
 #----------------------------------------------------------------------------------------------------#
 
-mkdir -p $compressedfolder
-chmod +x $compressedfolder
+mkdir -p $compressed_folder
+chmod +x $compressed_folder
 
 #****************************************************************************************************#
 #                                      TAKE SNAPSHOT OF CHAIN                                        #
@@ -57,14 +57,14 @@ if [[ $(($thehour%3)) -eq 0 ]] || [[ $testsnapshot -eq 1 ]]
 then
   echo "# Snapshot Only Start. Hour is $thehour #"
   snapname=$(curl http://127.0.0.1:8888/v1/producer/create_snapshot | jq '.snapshot_name')
-  rm -f $shcreate
-  touch $shcreate && chmod +x $shcreate
+  rm -f $sh_create
+  touch $sh_create && chmod +x $sh_create
   echo "tar -Scvzf $filename-snaponly.tar.gz $snapname" >> $shcreate
-  echo "ssh -p $sshportno $remote_user 'find $remote_server_folder -name \"*.gz\" -type f -size -1000k -delete'" >> $shcreate
-  echo "ssh -p $sshportno $remote_user 'ls -F $remote_server_folder/*.gz | head -n -8 | xargs -r rm'" >> $shcreate
-  echo "rsync -rv -e 'ssh -p $sshportno' --progress $filename-snaponly.tar.gz $remote_user:$remote_server_folder" >> $shcreate
+  echo "ssh -p $ssh_port $remote_user 'find $remote_server_folder -name \"*.gz\" -type f -size -1000k -delete'" >> $sh_create
+  echo "ssh -p $ssh_port $remote_user 'ls -F $remote_server_folder/*.gz | head -n -8 | xargs -r rm'" >> $sh_create
+  echo "rsync -rv -e 'ssh -p $ssh_port' --progress $file_name-snaponly.tar.gz $remote_user:$remote_server_folder" >> $sh_create
   echo "Sending snapshot only..."
-  $shcreate
+  $sh_create
 else
   echo "Snapshot is not due..Aborting"
 fi
@@ -77,7 +77,7 @@ fi
 # RUN TWICE A DAY BY MOD THE HOUR BY 12                                                              #
 #----------------------------------------------------------------------------------------------------#
 
-if [[ $(($thehour%12)) -eq 0 ]] || [[ $testblocks -eq 1 ]]
+if [[ $(($the_hour%12)) -eq 0 ]] || [[ $test_blocks -eq 1 ]]
 then
   echo "Get Head and Irreversible Block Numbers"
   head_block_num=$(cleos get info | jq '.head_block_num')
@@ -94,6 +94,7 @@ then
     echo "Last Irreversible Block Reached In $ans Blocks"
     sleep 10
   done
+  
   echo "Last Irreversible Block Number Passed - Great, lets stop the chain now"
 
 #----------------------------------------------------------------------------------------------------#
@@ -117,19 +118,19 @@ chainstopped=1
 #                                    COMPRESSING NODEOS BLOCKS                                       #
 #****************************************************************************************************#
 
-  rm -f $shcreatefull
-  touch $shcreatefull && chmod +x $shcreatefull
-  echo "tar -Scvzf $filename-blockslog.tar.gz $blocksfolder/blocks.log $blocksfolder/blocks.index" >> $shcreatefull
+  rm -f $sh_create_full
+  touch $sh_create_full && chmod +x $sh_create_full
+  echo "tar -Scvzf $file_name-blockslog.tar.gz $blocksfolder/blocks.log $blocks_folder/blocks.index" >> $sh_create_full
 
 #****************************************************************************************************#
 #                                    TRANSFERING NODEOS BLOCKS                                       #
 #****************************************************************************************************#
 
-  echo "ssh -p $sshportno $remote_user 'find $remote_server_folder/blocks -name \"*.gz\" -type f -size -1000k -delete'" >> $shcreate
-  echo "ssh -p $sshportno $remote_user 'ls -F $remote_server_folder/blocks/*.gz | head -n -1 | xargs -r rm'" >> $shcreatefull
-  echo "rsync -rv -e 'ssh -p $sshportno' --progress $filename-blockslog.tar.gz $remote_user:$remote_server_folder/blocks" >> $shcreatefull
+  echo "ssh -p $ssh_port $remote_user 'find $remote_server_folder/blocks -name \"*.gz\" -type f -size -1000k -delete'" >> $sh_create
+  echo "ssh -p $ssh_port $remote_user 'ls -F $remote_server_folder/blocks/*.gz | head -n -1 | xargs -r rm'" >> $sh_create_full
+  echo "rsync -rv -e 'ssh -p $ssh_port' --progress $file_name-blockslog.tar.gz $remote_user:$remote_server_folder/blocks" >> $sh_create_full
   echo "Sending blocks..."
-  $shcreatefull
+  $sh_create_full
 else
   echo "Blocks Log is not due..Aborting"
 fi
@@ -142,20 +143,19 @@ fi
 # RUN TWICE A DAY BY MOD THE HOUR BY 12                                                              #
 #----------------------------------------------------------------------------------------------------#
 
-if [[ $(($thehour%24)) -eq 0 ]] || [[ $teststatehistory -eq 1 ]]
+if [[ $(($the_hour%24)) -eq 0 ]] || [[ $test_state_history -eq 1 ]]
 then
-  echo "#State History Start #"
-  rm -f $shcreatefullstate
-  touch $shcreatefullstate && chmod +x $shcreatefullstate
-  echo "tar -Scvzf $filename-statehistory.tar.gz $statehistoryfolder  " >> $shcreatefullstate
+  rm -f $sh_create_fullstate
+  touch $sh_create_fullstate && chmod +x $sh_create_fullstate
+  echo "tar -Scvzf $file_name-state_history.tar.gz $state_history_folder  " >> $sh_create_fullstate
 
 #****************************************************************************************************#
 #                                    TRANSFERING NODEOS BLOCKS                                       #
 #****************************************************************************************************#
 
-  echo "ssh -p $sshportno $remote_user 'find $remote_server_folder/state-history -name \"*.gz\" -type f -size -1000k -delete'" >> $shcreate
-  echo "ssh -p $sshportno $remote_user 'ls -F $remote_server_folder/state-history/*.gz | head -n -1 | xargs -r rm'" >> $shcreatefullstate
-  echo "rsync -rv -e 'ssh -p $sshportno' --progress $filename-statehistory.tar.gz $remote_user:$remote_server_folder/state-history" >> $shcreatefullstate
+  echo "ssh -p $ssh_port $remote_user 'find $remote_server_folder/state-history -name \"*.gz\" -type f -size -1000k -delete'" >> $sh_create
+  echo "ssh -p $ssh_port $remote_user 'ls -F $remote_server_folder/state-history/*.gz | head -n -1 | xargs -r rm'" >> $sh_create_fullstate
+  echo "rsync -rv -e 'ssh -p $ssh_port' --progress $file_name-state_history.tar.gz $remote_user:$remote_server_folder/state-history" >> $sh_create_fullstate
   echo "Sending state history..."
   $shcreatefullstate
 else
@@ -166,20 +166,20 @@ fi
 #                                    SNAPSHOT CLEAN UP PROCESS                                       #
 #****************************************************************************************************#
 
-rm -f $shcreate
-rm -f $shcreatefull
-rm -f $shcreatefullstate
-rm -R $blocksfolder/*.gz
-rm -R $snapshotsfolder/*.gz
-rm -R $snapshotsfolder/*.bin
-rm -R $compressedfolder/*.gz
-rm -R $statehistoryfolder/*.gz
+rm -f $sh_create
+rm -f $sh_create_full
+rm -f $sh_create_fullstate
+rm -R $blocks_folder/*.gz
+rm -R $snapshots_folder/*.gz
+rm -R $snapshots_folder/*.bin
+rm -R $compressed_folder/*.gz
+rm -R $state_history_folder/*.gz
 
 #****************************************************************************************************#
 #                                  START NODEOS IN THE BACKGROUND                                    #
 #****************************************************************************************************#
 
-if [[ $chainstopped -eq 1 ]]
+if [[ $chain_stopped -eq 1 ]]
 then
 echo "Starting chain..."
 cd ~
