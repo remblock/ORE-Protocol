27b4bcb3be918292123d5d808b18c3003fcbfa81
SSDx <60996729+ssdx-remblock@users.noreply.github.com>
2020-08-28 13:47:28 +0930

    Update ore-take-snapshot.sh

--- ore-take-snapshot.sh
+++ ore-take-snapshot.sh
@@ -67,7 +67,7 @@ then
   echo ""
   echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'find $remote_server_folder -name \"*.gz\" -type f -size -1000k -delete'" >> $sh_create
   echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'ls -F $remote_server_folder/*.gz | head -n -8 | xargs -r rm'" >> $sh_create
-  echo "rsync -rv -e 'ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user ' --progress $file_name-snaponly.tar.gz $remote_user:$remote_server_folder" >> $sh_create
+  echo "rsync -rv -e 'ssh -i ~/.ssh/id_rsa -p $ssh_port '--progress $file_name-snaponly.tar.gz $remote_user:$remote_server_folder" >> $sh_create
   $sh_create
   echo ""
   echo "Snapshot transfer has now completed !!!"
