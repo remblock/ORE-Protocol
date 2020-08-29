8fb18ec602104aea8f4fd3addc5287e226d41e33
SSDx <60996729+ssdx-remblock@users.noreply.github.com>
2020-08-29 15:43:28 +0930

    Update ore-take-snapshot.sh

--- ore-take-snapshot.sh
+++ ore-take-snapshot.sh
@@ -67,7 +67,7 @@ then
   echo ""
   echo "$file_name.snaponly.tar.gz" > latestsnapshot.txt
   echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'find $remote_server_folder -name latestsnapshot.txt -type f -size -1000k -delete 2> /dev/null'"
-  echo "rsync -rv -e 'ssh -i ~/.ssh/id_rsa -p $ssh_port' --progress latestsnapshot.txt $remote_user:$remote_server_folder"
+  echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'find $remote_server_folder -name latestsnapshot.txt exec echo "$file_name.snaponly.tar.gz" > latestsnapshot.txt '{}' \;'"
   echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'find $remote_server_folder -name \"*.gz\" -type f -size -1000k -delete 2> /dev/null'" >> $sh_create
   echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'ls -F $remote_server_folder/*.gz | head -n -1 | xargs -r rm 2> /dev/null'" >> $sh_create
   echo "rsync -rv -e 'ssh -i ~/.ssh/id_rsa -p $ssh_port' --progress $file_name-snaponly.tar.gz $remote_user:$remote_server_folder" >> $sh_create
@@ -145,7 +145,7 @@ chain_stopped=1
 
   echo "$file_name-blockslog.tar.gz" > latestblocks.txt
   echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'find $remote_server_folder -name latestblocks.txt -type f -size -1000k -delete 2> /dev/null'"
-  echo "rsync -rv -e 'ssh -i ~/.ssh/id_rsa -p $ssh_port' --progress latestblocks.txt $remote_user:$remote_server_folder"
+  echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'find $remote_server_folder -name latestsnapshot.txt exec echo "$file_name-blockslog.tar.gz" > latestblocks.txt '{}' \;'"
   echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'find $remote_server_folder/blocks -name \"*.gz\" -type f -size -1000k -delete 2> /dev/null'" >> $sh_create_full
   echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'ls -F $remote_server_folder/blocks/*.gz | head -n -1 | xargs -r rm 2> /dev/null'" >> $sh_create_full
   echo "rsync -rv -e 'ssh -i ~/.ssh/id_rsa -p $ssh_port' --progress $file_name-blockslog.tar.gz $remote_user:$remote_server_folder/blocks" >> $sh_create_full
@@ -181,11 +181,12 @@ then
 
   echo "$file_name-state_history.tar.gz" > lateststatehistory.txt
   echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'find $remote_server_folder -name lateststatehistory.txt -type f -size -1000k -delete 2> /dev/null'"
-  echo "rsync -rv -e 'ssh -i ~/.ssh/id_rsa -p $ssh_port' --progress lateststatehistory.txt $remote_user:$remote_server_folder"
+  echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'find $remote_server_folder -name latestsnapshot.txt exec echo "$file_name-state_history.tar.gz" > lateststatehistory.txt '{}' \;'"
   echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'find $remote_server_folder/state-history -name \"*.gz\" -type f -size -1000k -delete 2> /dev/null'" >> $sh_create_fullstate
   echo "ssh -i ~/.ssh/id_rsa -p $ssh_port $remote_user 'ls -F $remote_server_folder/state-history/*.gz | head -n -1 | xargs -r rm 2> /dev/null'" >> $sh_create_fullstate
   echo "rsync -rv -e 'ssh -i ~/.ssh/id_rsa -p $ssh_port' --progress $file_name-state_history.tar.gz $remote_user:$remote_server_folder/state-history" >> $sh_create_fullstate
   $sh_create_fullstate
+  echo ""
   echo "Transfer of the State History has now completed"
   echo ""
 else
@@ -217,7 +218,6 @@ if [[ $chain_stopped -eq 1 ]]
 then
   cd ~
   nodeos  --config-dir ./config/ --disable-replay-opts --data-dir ./data/ >> nodeos.log 2>&1 &
-  echo ""
   echo "Started ORE-Protocol !!!"
   echo ""
 fi
