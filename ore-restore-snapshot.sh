7c9b4a125726d7e33c80bc978863bef4d55ad2b8
SSDx <60996729+ssdx-remblock@users.noreply.github.com>
2020-08-31 14:54:56 +0930

    Update ore-restore-snapshot.sh

--- ore-restore-snapshot.sh
+++ ore-restore-snapshot.sh
@@ -199,104 +199,46 @@ done
 rm -R $last_download_folder/*
 
 #----------------------------------------------------------------------------------------------------#
-# NODEOS STOP FUNCTION                                                                               #
+# MAIN PART OF THE SCRIPT                                                                            #
 #----------------------------------------------------------------------------------------------------#
 
-function StopNode() {
 nodeos_pid=$(pgrep nodeos)
-if [ ! -z "$nodeos_pid" ] 
+if [ ! -z "$nodeos_pid" ]
 then
-  if ps -p $nodeos_pid > /dev/null 
+  if ps -p $nodeos_pid > /dev/null
   then
     kill -SIGINT $nodeos_pid
   fi
-  while ps -p $nodeos_pid > /dev/null 
-  do
-    sleep 1
+  while ps -p $nodeos_pid > /dev/null; do
+  sleep 1
   done
 fi
-}
-
-#----------------------------------------------------------------------------------------------------#
-# NODEOS START FUNCTION                                                                              #
-#----------------------------------------------------------------------------------------------------#
-
-function StartNode() {
-  nodeos --config-dir $config_folder/ --disable-replay-opts --data-dir $data_folder/ >> $log_file 2>&1 &
-}
-
-#----------------------------------------------------------------------------------------------------#
-# NODEOS START SNAPSHOT FUNCTION                                                                     #
-#----------------------------------------------------------------------------------------------------#
-
-function StartNodeSnapshot() {
-  nodeos --config-dir $config_folder/ --disable-replay-opts --snapshot $snapshots_folder/$bin_file --data-dir $data_folder/ >> $log_file 2>&1 &
-}
-
-#----------------------------------------------------------------------------------------------------#
-# WRITE PERCENTAGE FUNCTION                                                                          #
-#----------------------------------------------------------------------------------------------------#
-
-function WritePercentage() {
-diff=$(($2-$1))
-sumit=$(awk "BEGIN {print ($diff/$2)*100}")
-percentage=$(awk "BEGIN {print (100 - $sumit) }")
-echo -en "\r$percentage% Chain Sync Completed\c\b"
-}
-
-#----------------------------------------------------------------------------------------------------#
-# STOP NODEOS AND RESTORE SNAPSHOT                                                                   #
-#----------------------------------------------------------------------------------------------------#
-
-StopNode
-sleep 1
+rm -rf $blocks_folder*/
+rm -rf $state_folder
 cd ~
-sleep 1
-echo ""
-StartNodeSnapshot
-WritePercentage
+nodeos --config-dir $config_folder --snapshot $bin_file --data-dir $data_folder >> $log_file 2>&1 &
+sleep 4
+while [ : ]
+do
+	systemdt=$(date '+%Y-%m-%dT%H:%M')
 
-#----------------------------------------------------------------------------------------------------#
-# CHECK IRREVERSIBLE BLOCK AGAINST EXTERNAL API                                                      #
-#----------------------------------------------------------------------------------------------------#
+	if [ "$dt1" == "$systemdt" ]; then
+		break
+	else
+		dt1=$(nodeos get info | grep head_block_time | cut -c 23-38)
+		dt1date=$(echo $dt1 | awk -F'T' '{print $1}' | awk -F'-' 'BEGIN {OFS="-"}{ print $3, $2, $1}')
+		dt1time=$(echo $dt1 | awk -F'T' '{print $2}' | awk -F':' 'BEGIN {OFS=":"}{ print $1, $2}')
 
-sync_log=/root/data/snapshots/sync.log
-touch $sync_log
-echo -999999999999 > $sync_log
-api_head_block_num=$(cleos -u $external_api get info | jq '.head_block_num')
-our_head_block_num=$(cleos get info | jq '.head_block_num')
-sleep 1
-block_diff=$(($api_head_block_num-$our_head_block_num))
-echo "
-echo "Head Block Number: $our_head_block_num"
-echo "API Head Block Number: $api_head_block_num"
-echo "Block Height Difference: $block_diff Blocks"
-echo ""
+		dt2=$(tail -n 1 $log_file | awk '{print $2}'| awk -F'.' '{print $1}')
+		dt2date=$(echo $dt2 | awk -F'T' '{print $1}' | awk -F'-' 'BEGIN {OFS="-"}{ print $3, $2, $1}')
+		dt2time=$(echo $dt2 | awk -F'T' '{print $2}' | awk -F':' 'BEGIN {OFS=":"}{ print $1, $2}')
 
-#----------------------------------------------------------------------------------------------------#
-# NOW WE WAIT FOR LAST IRREVERSIBLE BLOCK TO PASS OUR SNAPSHOT TAKEN                                 #
-#----------------------------------------------------------------------------------------------------#
+		echo "Fetching blocks for [${dt1date} | ${dt1time}] | Current block date [${dt2date} | ${dt2time}]"
+	fi
 
-if [[ $our_head_block_num -eq $api_head_block_num ]]
-then
-  echo 0 > $sync_log
-else
-  while [[ 1 -eq 1 ]]
-  do
-  api_head_block_num=$(cleos -u $external_api get info | jq '.head_block_num')
-  our_head_block_num=$(cleos get info | jq '.head_block_num')
-  block_diff=$(($api_head_block_num-$our_head_block_num))
-  echo $block_diff > $sync_log
-  if [[ $api_head_block_num -le $our_head_block_num ]]
-  then
-    break
-  else
-    WritePercentage $our_head_block_num $api_head_block_num
-    sleep 2
-  fi
-  done
-fi
-rm $sync_log
+	echo ""
+	sleep 2
+done
 echo ""
 echo "==================================="
 echo "ORE PROTOCOL SNAPSHOT HAS COMPLETED"
