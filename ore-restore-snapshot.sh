1963cd258b33c81eee2ede79fc917fcb3797038d
SSDx <60996729+ssdx-remblock@users.noreply.github.com>
2020-08-31 21:55:50 +0930

    Update ore-restore-snapshot.sh

--- ore-restore-snapshot.sh
+++ ore-restore-snapshot.sh
@@ -67,40 +67,6 @@ rm -rf $blocks_folder
 rm -rf $state_folder
 cd ~
 nodeos --config-dir $config_folder/ --data-dir $data_folder/ --snapshot $bin_file >> $log_file 2>&1 &
-sleep 4
-nodeos_pid=$(pgrep nodeos)
-if [ ! -z "$nodeos_pid" ]
-then
-  if ps -p $nodeos_pid > /dev/null; then
-     kill -SIGINT $nodeos_pid
-  fi
-  while ps -p $nodeos_pid > /dev/null; do
-  sleep 1
-  done
-fi
-nodeos --config-dir ./config/ --data-dir ./data/ >> nodeos.log 2>&1 &
-sleep 4
-while [ : ]
-do
-	systemdt=$(date '+%Y-%m-%dT%H:%M')
-
-	if [ "$dt1" == "$systemdt" ]; then
-		break
-	else
-		dt1=$(nodeos get info | grep head_block_time | cut -c 23-38)
-		dt1date=$(echo $dt1 | awk -F'T' '{print $1}' | awk -F'-' 'BEGIN {OFS="-"}{ print $3, $2, $1}')
-		dt1time=$(echo $dt1 | awk -F'T' '{print $2}' | awk -F':' 'BEGIN {OFS=":"}{ print $1, $2}')
-
-		dt2=$(tail -n 1 $log_file | awk '{print $2}'| awk -F'.' '{print $1}')
-		dt2date=$(echo $dt2 | awk -F'T' '{print $1}' | awk -F'-' 'BEGIN {OFS="-"}{ print $3, $2, $1}')
-		dt2time=$(echo $dt2 | awk -F'T' '{print $2}' | awk -F':' 'BEGIN {OFS=":"}{ print $1, $2}')
-
-		echo "Fetching blocks for [${dt1date} | ${dt1time}] | Current block date [${dt2date} | ${dt2time}]"
-	fi
-
-	echo ""
-	sleep 2
-done
 echo ""
 echo "==================================="
 echo "ORE PROTOCOL SNAPSHOT HAS COMPLETED"
