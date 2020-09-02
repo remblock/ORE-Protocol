7c4ab1703a5215df3e6463e8ffc3b3036ac7435f
SSDx <60996729+ssdx-remblock@users.noreply.github.com>
2020-09-03 06:47:18 +0930

    Update ore-fullnode-setup.sh

--- ore-fullnode-setup.sh
+++ ore-fullnode-setup.sh
@@ -77,7 +77,7 @@ function get_user_answer_yn(){
 echo -e "#-------------------------------------------------------------------------------" > $config_file
 echo -e "\nplugin = eosio::net_plugin\nplugin = eosio::chain_plugin\n\nhttp-server-address = 0.0.0.0:8888\n" >> $config_file
 echo -e "#-------------------------------------------------------------------------------" >> $config_file
-echo -e "\nchain-threads = 8\ntrace-history = true\neos-vm-oc-enable = true\nwasm-runtime = eos-vm-jit\nchain-state-history = true\nverbose-http-errors = true\nhttp-validate-host = false\neos-vm-oc-compile-threads = 8\nhttp-max-response-time-ms = 100\nchain-state-db-size-mb = 100480\nhttp-server-address = 0.0.0.0:80\nhttps-server-address = 0.0.0.0:443\np2p-listen-endpoint = 0.0.0.0:9876\nabi-serializer-max-time-ms = 15000\nstate-history-dir = "/root/data/shpdata"\nstate-history-endpoint = 0.0.0.0:8080\n" >> $config_file
+echo -e "\nchain-threads = 8\ntrace-history = true\neos-vm-oc-enable = true\nwasm-runtime = eos-vm-jit\nchain-state-history = true\nverbose-http-errors = true\nhttp-validate-host = false\neos-vm-oc-compile-threads = 8\nhttp-max-response-time-ms = 100\nchain-state-db-size-mb = 100480\nhttp-server-address = 0.0.0.0:80\nhttps-server-address = 0.0.0.0:443\np2p-listen-endpoint = 0.0.0.0:9876\nabi-serializer-max-time-ms = 15000\nstate-history-endpoint = 0.0.0.0:8080\nstate-history-dir = /root/data/shpdata\n" >> $config_file
 echo -e "#-------------------------------------------------------------------------------\n" >> $config_file
 wget https://github.com/remblock/ORE-Protocol/raw/master/ore-peer-list.ini
 cat /root/ore-peer-list.ini >> $config_file
