2104acf8cb2e9bd78deef9180b8ba69da412e058
SSDx <60996729+ssdx-remblock@users.noreply.github.com>
2020-09-03 15:49:07 +0930

    Update ore-producer-setup.sh

--- ore-producer-setup.sh
+++ ore-producer-setup.sh
@@ -73,21 +73,21 @@ sudo timedatectl set-timezone UTC
 # CONFIGURATION FILE (CONFIG/CONFIG.INI)                                                             #
 #----------------------------------------------------------------------------------------------------#
 
-echo -e "#-------------------------------------------------------------------------------" > $config_file
+echo -e "#------------------------------------------------------------------------------#" > $config_file
 echo -e "# EOSIO PLUGINS                                                                #" >> $config_file
-echo -e "#-------------------------------------------------------------------------------" >> $config_file
+echo -e "#------------------------------------------------------------------------------#" >> $config_file
 echo -e "\nplugin = eosio::net_plugin\nplugin = eosio::chain_plugin\nplugin = eosio::producer_plugin\nplugin = eosio::chain_api_plugin\n" >> $config_file
-echo -e "#-------------------------------------------------------------------------------" >> $config_file
+echo -e "#------------------------------------------------------------------------------#" >> $config_file
 echo -e "# CONFIG SETTINGS                                                              #" >> $config_file
-echo -e "#-------------------------------------------------------------------------------" >> $config_file
+echo -e "#------------------------------------------------------------------------------#" >> $config_file
 echo -e "\nmax-clients = 50\nchain-threads = 8\nsync-fetch-span = 200\neos-vm-oc-enable = false\npause-on-startup = false\nwasm-runtime = eos-vm-jit\nmax-transaction-time = 30\nverbose-http-errors = true\nkeosd-provider-timeout = 5\ntxn-reference-block-lag = 0\neos-vm-oc-compile-threads = 8\nconnection-cleanup-period = 30\nchain-state-db-size-mb = 100480\nenable-stale-production = false\nmax-irreversible-block-age = -1\nhttp-server-address = 0.0.0.0:8888\nreversible-blocks-db-size-mb = 10480\n" >> $config_file
-echo -e "#-------------------------------------------------------------------------------" >> $config_file
+echo -e "#------------------------------------------------------------------------------#" >> $config_file
 echo -e "# PRODUCER SETTINGS                                                            #" >> $config_file
-echo -e "#-------------------------------------------------------------------------------" >> $config_file
+echo -e "#------------------------------------------------------------------------------#" >> $config_file
 echo -e "\nproducer-name = $producer\nsignature-provider = $bp_public_key=KEY:$bp_private_key\n" >> $config_file
-echo -e "#-------------------------------------------------------------------------------" >> $config_file
+echo -e "#------------------------------------------------------------------------------#" >> $config_file
 echo -e "# ORE PROTOCOL P2P PEER ADDRESSES                                              #" >> $config_file
-echo -e "#-------------------------------------------------------------------------------\n" >> $config_file
+echo -e "#------------------------------------------------------------------------------#\n" >> $config_file
 wget https://github.com/remblock/ORE-Protocol/raw/master/ore-peer-list.ini
 cat /root/ore-peer-list.ini >> $config_file
 echo -e "\n#-------------------------------------------------------------------------------" >> $config_file
