ad93df37495b8b674f03e30bd6964f9d0ab47c2a
SSDx <60996729+ssdx-remblock@users.noreply.github.com>
2020-09-04 21:39:41 +0930

    Update ore-producer-setup.sh

--- ore-producer-setup.sh
+++ ore-producer-setup.sh
@@ -26,6 +26,7 @@ create_data_dir=/root/data
 state_dir=/root/data/state
 contact=contact@remblock.io
 blocks_dir=/root/data/blocks
+new_hostname=ore-bp1-remblock
 create_config_dir=/root/config
 nodeos_log_file=/root/nodeos.log
 config_file=/root/config/config.ini
@@ -50,7 +51,9 @@ fi
 # ADJUSTING SERVER HOSTNAME                                                                          #
 #----------------------------------------------------------------------------------------------------#
 
-sudo hostnamectl set-hostname ore.bp1.remblock
+old_hostname=$(hostname)
+sudo hostnamectl set-hostname $new_hostname
+sed -i "s/$old_hostname/$new_hostname/g" /etc/hosts
 
 #----------------------------------------------------------------------------------------------------#
 # UPDATING AND UPGRADING PACKAGE DATABASE                                                            #
